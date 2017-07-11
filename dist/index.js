54b9d1d1f30e74db9138fb8457147f7b06dc5fa7
Matt Hamlin <hamlim@outlook.com>
2017-07-10 20:05:32 -0400

    Adding a build step, gitignoring npm deps, plus dist files

---
+++ dist/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _container = require('./container.js');
+
+Object.defineProperty(exports, 'Container', {
+  enumerable: true,
+  get: function get() {
+    return _container.Container;
+  }
+});
+
+var _utils = require('./utils.js');
+
+Object.defineProperty(exports, 'makeTheme', {
+  enumerable: true,
+  get: function get() {
+    return _utils.makeTheme;
+  }
+});
